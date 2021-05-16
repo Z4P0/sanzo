db44954b5f8f08a0de6f6d45f4b5d9c8669fe65a
Luis Rosario <luis.rosario@rochester.edu>
2021-05-16 12:37:16 -0400

    configure 11ty

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy('site/scripts');
+  eleventyConfig.addPassthroughCopy('site/styles/*.css');
+
+  return {
+    dir: {
+      input: 'site',
+      output: 'www'
+    }
+  }
+}
